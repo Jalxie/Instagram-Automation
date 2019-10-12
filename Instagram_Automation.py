7bab1b72e90758b6e5e7b7f0f64df0770fdbbaaf
Jalxiey <jalseyswag@gmail.com>
2019-10-13 01:48:53 +1100

    watchdog added

---
+++ Instagram_Automation.py
@@ -0,0 +1,16 @@
+
+import sys
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from instapy_cli import client
+
+
+# username = 'jalxiey_'
+# password = ''
+# image = 'posts/post1.png'
+# text = 'Upload from python'
+
+# with client(username, password) as cli:
+#     cli.upload(image, text)
